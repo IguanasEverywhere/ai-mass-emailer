1cd496d23e3ff44bb7aedbc676a91df8095c2af2
Scott Schwab <scottschwab86@gmail.com>
2024-06-25 10:32:52 -0500

    Set up stateful form inputs

--- ai-mass-emailer/src/components/GeneratorForm/GeneratorForm.js
+++ ai-mass-emailer/src/components/GeneratorForm/GeneratorForm.js
@@ -1,23 +1,48 @@
 import { useState } from 'react';
-import {getResponseContent} from '../../utils/openAICall';
+import { getResponseContent } from '../../utils/openAICall';
 import requestObj from '../../utils/requestObject';
 
 const GeneratorForm = () => {
 
   const [responseContent, setResponseContent] = useState({});
+  const [formContent, setFormContent] = useState({
+    "audience": "",
+    "topic": "",
+  })
 
   const handleGenerateClick = () => {
     getResponseContent(requestObj)
       .then(r => setResponseContent(JSON.parse(r.content)));
   }
 
+  const handleFormChange = (e) => {
+    setFormContent(prevVals => ({...prevVals, [e.target.name]:e.target.value}))
+  }
+
 
   return (
-    <><p>Generator Form</p>
-      <button type="button" onClick={handleGenerateClick}>Generate Email</button>
-      <p>{responseContent.content}</p>
+    <>
+      <form>
+        <label htmlFor="audience">Who's the email to?</label>
+        <input
+          type="text"
+          name="audience"
+          onChange={handleFormChange}
+          value={formContent.audience}></input>
+
+        <label htmlFor="topic">What's it about?</label>
+        <input
+          type="text"
+          name="topic"
+          onChange={handleFormChange}
+          value={formContent.topic}></input>
+
+        <button type="button" onClick={handleGenerateClick}>Generate Email</button>
+
+        <p>{responseContent.content}</p>
+      </form>
     </>
-    )
+  )
 
 }
 
